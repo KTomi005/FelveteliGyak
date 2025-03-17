96267503df894a590f2381d3dc96267da4716864
KTomi005 <kemtam197@hengersor.hu>
2025-03-17 20:30:15 +0100

    Update Home.jsx

--- Frontend/src/Oldalak/Home.jsx
+++ Frontend/src/Oldalak/Home.jsx
@@ -12,7 +12,7 @@ function Home(){
                         A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el. A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján rangsorolja az iskolába jelentkezőket. <br/><a href="https://www.oktatas.hu/kozneveles/kozepfoku_felveteli_eljaras/kozepfoku_felveteli_eljaras_informacioi" target='_blank'>Tájákoztató oldal</a>
                         <img src="logo.png"  className="img-thumbnail" alt="img-thumbnail" title="" /></p>
                     </div>
-                    <div className="col-md-4 bg-white p-3">
+                    <div className="col-md-4">
                         <h3>Tájékoztatás</h3> 
                         <h3>Jószakma Szakgimnázium </h3>
                         <p>A központi felvételit magyar nyelv és irodalom, illetve matematika tantárgyakból írják a jelentkezők. Mindkét tárgy esetén legfeljebb 50 pont szerezhető. A felvételiző hozott pontjait az általános iskolai év végi eredményei alapján számolják, ez a pontszám legfeljebb 50 pont lehet. A hozott pontokat duplázzák. A központi felvételin szerzett és a hozott pontok összege adja a felvételiző összesített pontszámát.</p><br/>
